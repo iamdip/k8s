66decf0e3243f941d315fe039ce32f78643a9e98
root <root@Dipak.localdomain>
2022-02-25 17:47:17 +0530

    add circle ci conf

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-docker build -t cygnetops/multi-client-k8s:latest -t 'dipakoo7/multi-client-k8s:$SHA' -f ./client/Dockerfile ./client
-docker build -t cygnetops/multi-server-k8s-pgfix:latest -t 'dipakoo7/multi-server-k8s-pgfix:$SHA' -f ./server/Dockerfile ./server
-docker build -t cygnetops/multi-worker-k8s:latest -t 'dipakoo7/multi-worker-k8s:$SHA' -f ./worker/Dockerfile ./worker
+docker build -t cygnetops/multi-client-k8s:latest -t 'dipakoo7/multi-client-k8s' -f ./client/Dockerfile ./client
+docker build -t cygnetops/multi-server-k8s-pgfix:latest -t 'dipakoo7/multi-server-k8s-pgfix' -f ./server/Dockerfile ./server
+docker build -t cygnetops/multi-worker-k8s:latest -t 'dipakoo7/multi-worker-k8s' -f ./worker/Dockerfile ./worker
 
 docker push dipakoo7/multi-client-k8s:latest
 docker push dipakoo7/multi-server-k8s-pgfix:latest
 docker push dipakoo7/multi-worker-k8s:latest
 
-docker push dipakoo7/multi-client-k8s:$SHA
-docker push dipakoo7/multi-server-k8s-pgfix:$SHA
-docker push dipakoo7/multi-worker-k8s:$SHA
+docker push dipakoo7/multi-client-k8s
+docker push dipakoo7/multi-server-k8s-pgfix
+docker push dipakoo7/multi-worker-k8s
 
 #kubectl apply -f k8s
 #kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
